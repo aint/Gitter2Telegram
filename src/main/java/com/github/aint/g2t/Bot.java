48dfd034f4f5b23c40bb58882c497e5350559873
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2017-12-25 23:25:43 +0200

    Init

---
+++ src/main/java/com/github/aint/g2t/Bot.java
@@ -0,0 +1,52 @@
+package com.github.aint.g2t;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText() + ", echo");
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "gitter2telegram_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "token";
+    }
+}
